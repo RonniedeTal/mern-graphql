2ed08eccf24ba2eefd897a9d623e4f5285f4e356
RonniedeTal <ronnievelz@gmail.com>
2025-12-01 13:46:36 +0100

    repairing deploy

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ graphiql: process.env.NODE_ENV === 'development'
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
+  app.get(/^\/(?!graphql).*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 }
